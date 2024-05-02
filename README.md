e9dc6305b196849700ebc452d70b3ba95c46abbd
Orçun Çolak <orcun.colak@hazelcast.com>
2024-05-02 14:08:40 +0300

    timeout example

--- README.md
+++ README.md
@@ -3,6 +3,11 @@
 The original idea is from  
 https://medium.com/@mustafaguc/building-kafka-producer-and-consumer-microservices-with-spring-boot-on-kubernetes-using-github-0bd0af37e538
 
+# Timeout RestClient
+
+The original idea is from  
+https://dzone.com/articles/timeout-in-spring-boot-with-restclient-webclient-a
+
 # Redirecting RestClient
 
 The original idea is from  
--- src/main/java/com/colak/springrestclienttutorial/config/RestClientConfig.java
+++ src/main/java/com/colak/springrestclienttutorial/config/RestClientConfig.java
@@ -3,6 +3,7 @@ package com.colak.springrestclienttutorial.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.ClientHttpRequestInterceptor;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.RestClient;
 import org.springframework.web.client.RestTemplate;
@@ -20,6 +21,20 @@ public class RestClientConfig {
         List<ClientHttpRequestInterceptor> interceptorList = List.of(RestTemplateConfig.interceptor());
         restTemplate.setInterceptors(interceptorList);
         return RestClient.create(restTemplate);
+    }
+
+    @Bean
+    public RestClient timeoutRestClient() {
+        var timeout = 5_000;
+        //  ClientHttpRequestFactory that uses Apache HttpComponents HttpClient
+        HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory();
+        factory.setConnectTimeout(timeout);
+        factory.setConnectionRequestTimeout(timeout);
+
+        return RestClient
+                .builder()
+                .requestFactory(factory)
+                .build();
     }
 
     /**
